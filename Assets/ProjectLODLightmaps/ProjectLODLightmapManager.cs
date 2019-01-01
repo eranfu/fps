7e2dd7254ff8c41ed0cd312af123cb540f96d62d
mango0110 <1119369173@qq.com>
2019-01-01 09:33:19 +0800

    level 01 background scene

---
+++ Assets/ProjectLODLightmaps/ProjectLODLightmapManager.cs
@@ -0,0 +1,50 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEditor;
+
+namespace ProjectLODLightmaps
+{
+    [ExecuteInEditMode]
+    public class ProjectLODLightmapManager : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+            Lightmapping.completed += SetupRenderer;
+        }
+
+        private void OnDisable()
+        {
+            EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+            Lightmapping.completed -= SetupRenderer;
+        }
+
+        private void Start()
+        {
+            SetupRenderer();
+        }
+
+        private void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            if (state == PlayModeStateChange.ExitingPlayMode)
+            {
+                SetupRenderer();
+            }
+        }
+
+        private void SetupRenderer()
+        {
+            Profiler.BeginSample("ProjectLODLightmapManager.SetupRender");
+            var projectors = FindObjectsOfType<ProjectLODLightmaps>();
+            foreach (var projector in projectors)
+            {
+                projector.SetupRenderer();
+            }
+
+            Profiler.EndSample();
+        }
+    }
+}
+
+#endif
---
+++ Assets/ProjectLODLightmaps/ProjectLODLightmaps.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+
+namespace ProjectLODLightmaps
+{
+    public class ProjectLODLightmaps : MonoBehaviour
+    {
+        public new Renderer renderer;
+
+        public void SetupRenderer()
+        {
+            var r = GetComponent<Renderer>();
+            if (r && this.renderer)
+            {
+                r.lightmapScaleOffset = this.renderer.lightmapScaleOffset;
+                r.lightmapIndex = this.renderer.lightmapIndex;
+            }
+        }
+    }
+}
