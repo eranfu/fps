4b308a08649a505aa42986dfefb64631371869eb
mango0110 <1119369173@qq.com>
2019-02-09 00:17:34 +0800

    WeakAssetReferenceDrawer

---
+++ Assets/Scripts/Utils/WeakAssetReference/Editor/WeakAssetReferenceDrawer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Utils.WeakAssetReference.Editor
+{
+    [CustomPropertyDrawer(typeof(WeakAssetReference))]
+    public class WeakAssetReferenceDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var assetTypeAttribute =
+                Attribute.GetCustomAttribute(fieldInfo, typeof(AssetTypeAttribute)) as AssetTypeAttribute;
+            Type assetType = assetTypeAttribute?.type ?? typeof(GameObject);
+
+            SerializedProperty guid = property.FindPropertyRelative("guid");
+            string assetPath = AssetDatabase.GUIDToAssetPath(guid.stringValue);
+            Object obj = AssetDatabase.LoadAssetAtPath(assetPath, assetType);
+
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive),
+                new GUIContent(label.text + "(" + guid.stringValue + ")"));
+            Object newObj = EditorGUI.ObjectField(position, obj, assetType, false);
+            if (newObj != obj)
+            {
+                if (newObj != null)
+                {
+                    assetPath = AssetDatabase.GetAssetPath(newObj);
+                    property.stringValue = AssetDatabase.AssetPathToGUID(assetPath);
+                }
+                else
+                {
+                    property.stringValue = "";
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/Utils/WeakAssetReference/WeakAssetReference.cs
+++ Assets/Scripts/Utils/WeakAssetReference/WeakAssetReference.cs
@@ -1,14 +1,25 @@
 ﻿#if UNITY_EDITOR
 using UnityEditor;
-
 #endif
+using System;
 
 namespace Utils.WeakAssetReference
 {
+    [AttributeUsage(AttributeTargets.Field)]
+    public class AssetTypeAttribute : Attribute
+    {
+        public readonly Type type;
+
+        public AssetTypeAttribute(Type type)
+        {
+            this.type = type;
+        }
+    }
+
     [System.Serializable]
     public class WeakAssetReference
     {
-        public string guid;
+        public string guid = "";
 
 #if UNITY_EDITOR
         public T LoadAsset<T>() where T : UnityEngine.Object
