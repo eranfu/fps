917fff905e851548a8c9510753de904b756da7c1
fh9501 <1119369173@qq.com>
2019-02-11 17:54:46 +0800

    BufferPool

---
+++ Assets/Scripts/Utils/Pool/BufferPool.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils.Pool
+{
+    public static class BufferPool
+    {
+        public static T[] Pop<T>(int minLength = 16)
+        {
+            return Inner<T>.Pop(minLength);
+        }
+
+        public static void Push<T>(T[] buffer)
+        {
+            Inner<T>.Push(buffer);
+        }
+
+        private static class Inner<T>
+        {
+            private static readonly Stack<T[]> Pool = new Stack<T[]>(1);
+
+            public static T[] Pop(int minLength)
+            {
+                while (Pool.Count > 0)
+                {
+                    T[] top = Pool.Pop();
+                    if (top.Length >= minLength)
+                        return top;
+                }
+
+                return new T[minLength];
+            }
+
+            public static void Push(T[] buffer)
+            {
+                Debug.Assert(buffer != null);
+                Array.Clear(buffer, 0, buffer.Length);
+                Pool.Push(buffer);
+            }
+        }
+    }
+}
