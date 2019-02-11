9995db86316c61375f712137cabd95f1bee78709
fh9501 <1119369173@qq.com>
2019-02-11 16:39:30 +0800

    ConversionUtility

---
+++ Assets/Scripts/Utils/ConversionUtility.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Utils
+{
+    public class ConversionUtility
+    {
+        public static float UInt32ToFloat(uint value)
+        {
+            return new UIntFloat {uintValue = value}.floatValue;
+        }
+
+        public static uint FloatToUInt32(float value)
+        {
+            return new UIntFloat {floatValue = value}.uintValue;
+        }
+
+        public static double ULongToDouble(ulong value)
+        {
+            return new ULongDouble() {ulongValue = value}.doubleValue;
+        }
+
+        public static ulong DoubleToULong(double value)
+        {
+            return new ULongDouble {doubleValue = value}.ulongValue;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct UIntFloat
+        {
+            [FieldOffset(0)] public float floatValue;
+            [FieldOffset(0)] public uint uintValue;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        private struct ULongDouble
+        {
+            [FieldOffset(0)] public double doubleValue;
+            [FieldOffset(0)] public ulong ulongValue;
+        }
+    }
+}
